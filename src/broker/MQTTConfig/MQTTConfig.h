b00c46f5377dc3af6d3628b3480678c0b2a3e059
bajacqueli <bastien.jacquelin@etu.uca.fr>
2025-10-08 23:21:17 +0200

    Update MQTT configuration and add WiFi configuration header
    - Changed MQTT broker IP from "192.168.100.34" to "192.168.1.80" in MQTTConfig.h.
    - Added a new header file WifiConfig-model.h for WiFi SSID and password configuration.
    - Updated project checksum to reflect changes.

--- src/broker/MQTTConfig/MQTTConfig.h
+++ src/broker/MQTTConfig/MQTTConfig.h
@@ -3,7 +3,7 @@
 
 #define MQTT_BROKER "mosquitto"
 #define MQTT_BROKER_PORT 1883
-#define MQTT_BROKER_IP "192.168.100.34"
+#define MQTT_BROKER_IP "192.168.1.80"
 #define MQTT_TOPIC "esp32/sensor"
 #define MQTT_LED_TOPIC "esp32/led"
 
---
+++ src/broker/WiFiConfig/WifiConfig-model.h
@@ -0,0 +1,7 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+#define WIFI_SSID "your_SSID"
+#define WIFI_PASSWORD "your_PASSWORD"
+
+#endif
